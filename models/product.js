bd187dc598a11f077b5c288d03da328634c36eb8
jnferreira <nunoferreira840@gmail.com>
2021-04-25 21:00:36 +0100

    imageUrl in product model

--- models/product.js
+++ models/product.js
@@ -3,7 +3,8 @@ let Schema = mongoose.Schema;
 
 let productSchema = new Schema({
     price: {type: Number, required: true},
-    name: {type: String, required: true}
+    name: {type: String, required: true},
+    imageUrl: {type: String, required: false}
 });
 
 let Product = mongoose.model('products', productSchema);
