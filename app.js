46a205a73abfa65b24933af9a6d8a434116adb4a
David Mata <52453479+davidrobert99@users.noreply.github.com>
2021-04-27 02:02:54 +0100

    Update

--- app.js
+++ app.js
@@ -19,23 +19,56 @@ app.use(bodyParser.json());
 //############################################################################################################################################
 //############################################################################################################################################
 
-let key = new rsa({b: 512});
-let publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKrTop707ZoaedMNI+Eas4kRP6Z2fT0AGBLZyRbGVxUO\n" +
-    "    d8YH1lveX7DB1QkcXZ7M/jWv83mPR8I5lzu0hnl/tj8CAwEAAQ=="
-let publicK = "-----BEGIN PUBLIC KEY-----\n"+publicKey+"\n"+"-----END PUBLIC KEY-----";
-//key.importKey(publicK,"pkcs8-public-pem");
-let signature = "nNQ4NvMHoaWoCDs9SjkEwi8BXfudv9xeW38Kk/Cf/d84Nydop6OqRy+Tjlwk3axZYqxPiH8nzZbF8Cr/MNeVrg=="
+var publicKey = '-----BEGIN PUBLIC KEY-----\n'+
+'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCtxfK4QUfRF1ej5+FlRb6wk8taLICBNbtqver9M6WzAY7JwbTqH5Mz9+9CMeKCDILoJpIHWVXPgmq+UvwXtH/3c09CYXqRv4WDp+HJA37jlnPW1tJNjkWnN8CC4MixSUq3I8JJ501Ff5c3vc03Ws7OSZUUua/PTpnHUUMDKU4BSQ5576S2413YSTvl6ggNhNRCUAppLyMRteO/0ykyL2IUO/vdvPF/o8Gg0fOlldx6px1MQURYFAW9TPJe42UUgKWtH9gQ06TjhCbUKLTnPI8QbWRB/6hYiQ6rgAX+G85Bm6FR2mV5O6ScLCz9o9uHXSCdRwUlg1WUFTpgpG4Zt4wIDAQAB\n'+
+'-----END PUBLIC KEY-----';
 
-let verifier = crypto.createVerify('RSA-SHA256');
-// verifier.update(sign);
-let ver = verifier.verify(publicK, signature,'base64');
+var mensagem = '{"userId": "608573888915f60015e1c445", "products": [{"_id": "Teste", "name": "test", "price": 0.3586371799442135, "quantity": 1}], "totalPrice": 0.3586371799442135}';
+
+var sign = "Bca5lrvF0Rxha33H6fhuAbvD72ST6DOMscBmIuRqJxEhcUg9Zg0n1L19dtnENkb99APm6AjKiYpSb6UNaL9xLZ6YUeppTTFUEKoQEIpwxLTpKCX3oMA073vOY9Gbr9bUaGSRtRilVjIZLeG//xwqXgpGttWnv44BYxAzGpD4Jz8xh2Hb1cYZlfr8RUlCWhPxx+LHogr9+yawJJmxddjYfmFAQk7Kgjf2jjaFElDdPqPWcMLnJg98F+R0g4F/QGHqWBZgw0ejB9K2ucuiCQg3euTyFfNxqpTqCqZzPc8RuDsnDmPXuruet9oiXaK06OEH9Yv/6qyDYNm6wn03CxqLwQ==";
+
+var verifier = crypto.createVerify('sha256');
+verifier.update(mensagem);
+var ver = verifier.verify(publicKey, sign,'base64');
 console.log(ver);
 
-let verifierObject = crypto.createVerify("RSA-SHA256");
-verifierObject.update(" ");
-let verified = verifierObject.verify({key:publicK, padding:crypto.constants.RSA_PKCS1_PSS_PADDING}, signature, "base64");
-console.info("is signature ok?: %s", verified);
 
+
+/*
+var privateKey = '-----BEGIN RSA PRIVATE KEY-----\n'+
+'MIICXQIBAAKBgQDCtTEic76GBqUetJ1XXrrWZcxd8vJr2raWRqBjbGpSzLqa3YLv\n'+
+'VxVeK49iSlI+5uLX/2WFJdhKAWoqO+03oH4TDSupolzZrwMFSylxGwR5jPmoNHDM\n'+
+'S3nnzUkBtdr3NCfq1C34fQV0iUGdlPtJaiiTBQPMt4KUcQ1TaazB8TzhqwIDAQAB\n'+
+'AoGAM8WeBP0lwdluelWoKJ0lrPBwgOKilw8W0aqB5y3ir5WEYL1ZnW5YXivS+l2s\n'+
+'tNELrEdapSbE9hieNBCvKMViABQXj4DRw5Dgpfz6Hc8XIzoEl68DtxL313EyouZD\n'+
+'jOiOGWW5UTBatLh05Fa5rh0FbZn8GsHrA6nhz4Fg2zGzpyECQQDi8rN6qhjEk5If\n'+
+'+fOBT+kjHZ/SLrH6OIeAJ+RYstjOfS0bWiM9Wvrhtr7DZkIUA5JNsmeANUGlCrQ2\n'+
+'cBJU2cJJAkEA26HyehCmnCkCjit7s8g3MdT0ys5WvrAFO6z3+kCbCAsGS+34EgnF\n'+
+'yz8dDdfUYP410R5+9Cs/RkYesqindsvEUwJBALCmQVXFeKnqQ99n60ZIMSwILxKn\n'+
+'Dhm6Tp5Obssryt5PSQD1VGC5pHZ0jGAEBIMXlJWtvCprScFxZ3zIFzy8kyECQQDB\n'+
+'lUhHVo3DblIWRTVPDNW5Ul5AswW6JSM3qgkXxgHfYPg3zJOuMnbn4cUWAnnq06VT\n'+
+'oHF9fPDUW9GK3yRbjNaJAkAB2Al6yY0KUhYLtWoEpQ40HlATbhNel2cn5WNs6Y5F\n'+
+'2hedvWdhS/zLzbtbSlOegp00d2/7IBghAfjAc3DE9DZw\n'+
+'-----END RSA PRIVATE KEY-----';
+
+var publicKey = '-----BEGIN PUBLIC KEY-----\n'+
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCtTEic76GBqUetJ1XXrrWZcxd\n'+
+'8vJr2raWRqBjbGpSzLqa3YLvVxVeK49iSlI+5uLX/2WFJdhKAWoqO+03oH4TDSup\n'+
+'olzZrwMFSylxGwR5jPmoNHDMS3nnzUkBtdr3NCfq1C34fQV0iUGdlPtJaiiTBQPM\n'+
+'t4KUcQ1TaazB8TzhqwIDAQAB\n'+
+'-----END PUBLIC KEY-----';
+
+var signer = crypto.createSign('sha256WithRSAEncryption');
+signer.update('hola');
+var sign = signer.sign(privateKey,'base64');
+
+var verifier = crypto.createVerify('sha256WithRSAEncryption');
+verifier.update('hola');
+var ver = verifier.verify(publicKey, sign,'base64');
+console.log(ver);
+
+
+*/
 //############################################################################################################################################
 //############################################################################################################################################
 //############################################################################################################################################
